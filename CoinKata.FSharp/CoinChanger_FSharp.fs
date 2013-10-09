be49092a5ae415c437d7d87e869e5e68ab41d088
Carsten König <Carsten@gettingsharper.de>
2013-10-09 13:20:25 +0200

    F# Implementierung hinzugefgt

---
+++ CoinKata.FSharp/CoinChanger_FSharp.fs
@@ -0,0 +1,27 @@
+﻿namespace CoinKata.FSharp
+
+open System
+open CoinKata
+
+type Münze = int
+type Cents = int
+type AnzahlMünzePaar = int * Münze
+type Wechselgeld = AnzahlMünzePaar list
+
+type CoinChanger_FSharp() = 
+
+    let ausgabeUndRestbetragFürMünze (bisher : Wechselgeld, restBetrag : Cents) 
+                                     (aktuelleMünze : Münze) =
+        let (anzahl, restBetrag') = Math.DivRem(restBetrag, aktuelleMünze)
+        let neuerWechsel = (anzahl, aktuelleMünze) :: bisher
+        (neuerWechsel, restBetrag')
+
+    let berechneWechselgeld münzen betrag =
+        let initial = ([], betrag)
+        münzen |> Seq.fold ausgabeUndRestbetragFürMünze initial
+               |> fst
+
+    interface ICoinChanger with
+        member i.BerechneWechselgeld(verfügbareMünzen, betrag) = 
+            berechneWechselgeld verfügbareMünzen betrag
+            |> List.toSeq
---
+++ CoinKata.Tests/CoinChanger_FSharp.cs
@@ -0,0 +1,6 @@
+﻿namespace CoinKata.Tests
+{
+    public class CoinChanger_FSharp : CoinChanger_Tests<CoinKata.FSharp.CoinChanger_FSharp>
+    {
+    }
+}
